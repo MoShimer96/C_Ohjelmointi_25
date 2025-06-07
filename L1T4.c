fb9ebe39f80f42483abd7c8c6f8a8dd2380cdc3c
MoShimer96 <moh.shimer@gmail.com>
2025-06-07 12:10:34 +0300

    Fixed L1T4.c

--- L1T4.c
+++ L1T4.c
@@ -8,12 +8,12 @@ int main(void){
 	printf("Anna merkki: ");
 	scanf("%c", &merkki);
 	getchar();
-	printf("Annoit merkin '%c'.", merkki);
+	printf("Annoit merkin '%c'.\n", merkki);
 
 	printf("Anna korkeintaan 20 merkkiä pitkä merkkijono: ");
 	fgets(merkkijono, 20, stdin);
 	merkkijono[strlen(merkkijono)-1] = '\0';
-	printf("Annoit merkkijono '%s'", merkkijono);
+	printf("Annoit merkkijono '%s'.", merkkijono);
 
 	return(0);
 }
