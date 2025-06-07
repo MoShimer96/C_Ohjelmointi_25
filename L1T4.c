082798685c4978bc9ebf82cfd98d9bb1fdea7379
MoShimer96 <moh.shimer@gmail.com>
2025-06-07 12:04:38 +0300

    test

---
+++ L1T4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	char merkki;
+	char merkkijono[21];
+
+	printf("Anna merkki: ");
+	scanf("%c", &merkki);
+	getchar();
+	printf("Annoit merkin '%c'.", merkki);
+
+	printf("Anna korkeintaan 20 merkkiä pitkä merkkijono: ");
+	fgets(merkkijono, 20, stdin);
+	merkkijono[strlen(merkkijono)-1] = '\0';
+	printf("Annoit merkkijono '%s'", merkkijono);
+
+	return(0);
+}
+
