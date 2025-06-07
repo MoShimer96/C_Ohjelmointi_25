8ba13271c4efd88dc854ab9591bead6f1f4b4384
MoShimer96 <moh.shimer@gmail.com>
2025-06-07 12:56:22 +0300

    Fixed eveything

---
+++ L1/L1T4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	char merkki;
+	char merkkijono[20];
+
+	printf("Anna merkki: ");
+	scanf("%c", &merkki);
+	getchar();
+	printf("Annoit merkin '%c'.\n", merkki);
+
+	printf("Anna korkeintaan 20 merkkiä pitkä merkkijono: ");
+	fgets(merkkijono, 20, stdin);
+	merkkijono[strlen(merkkijono)-1] = '\0';
+ 	printf("Annoit merkkijonon '%s'.", merkkijono);
+
+	return(0);
+}
