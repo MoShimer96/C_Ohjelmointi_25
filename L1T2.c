0bc555c0ee48408fdddbd790b2013d771579532d
MoShimer96 <135559911+MoShimer96@users.noreply.github.com>
2025-06-07 13:50:48 +0300

    L1T2.c

---
+++ L1T2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) { 
+int Kokonaisluku; 
+float Liukuluku; 
+printf("Anna kokonaisluku: "); 
+scanf("%d", &Kokonaisluku);
+printf("Anna liukuluku: ");
+scanf("%f" ,&Liukuluku); 
+printf("Annoit luvut %d ja %5.2f.\n", Kokonaisluku, Liukuluku); 
+return(0); 
+}                                                                                                                                                                  
